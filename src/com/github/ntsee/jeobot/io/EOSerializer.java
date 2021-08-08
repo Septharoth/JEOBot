2346ec9754dceeed9800061ba616940206a3a6c1
Septharoth <86432377+Septharoth@users.noreply.github.com>
2021-08-08 10:27:41 -0400

    unsigned division with Integer wrapper class

--- src/com/github/ntsee/jeobot/io/EOSerializer.java
+++ src/com/github/ntsee/jeobot/io/EOSerializer.java
@@ -172,17 +172,17 @@ public class EOSerializer {
         Arrays.fill(bytes, (byte)0xFE);
 
         if (length >= 4 && onumber >= THREE_BYTE_MAX) {
-            bytes[3] = (byte)(number / THREE_BYTE_MAX + 1);
+            bytes[3] = (byte)(Integer.divideUnsigned(number, THREE_BYTE_MAX) + 1);
             number %= THREE_BYTE_MAX;
         }
 
         if (length >= 3 && onumber >= TWO_BYTE_MAX) {
-            bytes[2] = (byte)(number / TWO_BYTE_MAX + 1);
+            bytes[2] = (byte)(Integer.divideUnsigned(number, TWO_BYTE_MAX) + 1);
             number %= TWO_BYTE_MAX;
         }
 
         if (length >= 2 && onumber >= ONE_BYTE_MAX) {
-            bytes[1] = (byte)(number / ONE_BYTE_MAX + 1);
+            bytes[1] = (byte)(Integer.divideUnsigned(number, ONE_BYTE_MAX) + 1);
             number %= ONE_BYTE_MAX;
         }
 
